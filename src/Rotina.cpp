be5d2b81ce20495941a6d789039727a215b6085a
aaronguizoli <aaronguizol@gmail.com>
2025-07-01 00:03:39 -0300

    Delete src/Rotina.cpp

--- src/Rotina.cpp
+++
@@ -1,5 +0,0 @@
-#include "Rotina.hpp"
-namespace ufmg_carona {
-    Rotina::Rotina(DiaDaSemana dia, const std::string& h, const std::string& l, const std::string& d) : _dia(dia), _horario_saida(h), _local_saida_padrao(l), _destino_final(d) {}
-    Rotina::Rotina() : _dia(DiaDaSemana::DOMINGO) {}
-}
